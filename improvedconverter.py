23d2316980c5ba5d82c188b8532d332dcb59b876
Xavier-Lleva <xaglleva@brc.pshs.edu.ph>
2026-08-20 09:37:05 +0800

    added spaces to the equal signs

--- improvedconverter.py
+++ improvedconverter.py
@@ -1,11 +1,11 @@
 # this block converts the given input, kilometers, to miles
-km =float(input("Enter the distance in kilometers: "))
-cf =0.621371 # this is the conversion factor because 1 km ≈ 0.621371 miles
+km = float(input("Enter the distance in kilometers: "))
+cf = 0.621371 # this is the conversion factor because 1 km ≈ 0.621371 miles
 m = km*cf
 print("Distance in miles:" ,m)
 
 # this gives them the option to do a second conversion
-sc =input('Do you want to convert another distance? (yes/no): ')
+sc = input('Do you want to convert another distance? (yes/no): ')
 if sc == "yes":
     # performs the second conversion 
     sckm = float(input("Enter the distance in kilometers: "))
